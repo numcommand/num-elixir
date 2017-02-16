e01d3357d4f83eda333f24d05c88b23df9becacb
Michael Pope <mpope.cr@gmail.com>
2017-02-16 15:16:20 -0800

    Create inital commit

---
+++ lib/num.ex
@@ -0,0 +1,18 @@
+defmodule Num do
+  @moduledoc """
+  Documentation for Num.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Num.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/num_test.exs
@@ -0,0 +1,8 @@
+defmodule NumTest do
+  use ExUnit.Case
+  doctest Num
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
